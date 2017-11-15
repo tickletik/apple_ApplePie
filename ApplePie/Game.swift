948cf956c31ed279a1012ea3b92a5d8430438509
Ronny Abraham <this.ronny@gmail.com>
2017-11-15 12:31:44 +0200

    add array guessedLetters and func playerGuessed to Game

--- ApplePie/Game.swift
+++ ApplePie/Game.swift
@@ -11,4 +11,13 @@ import Foundation
 struct Game {
     var word:String
     var incorrectMovesRemaining:Int
+    var guessedLetters: [Character]
+
+    mutating func PlayerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+
+        if !word.characters.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
 }
