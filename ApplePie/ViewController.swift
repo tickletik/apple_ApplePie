fb661f75f19edafa0f9db0db9989112ee5d2a758
Ronny Abraham <this.ronny@gmail.com>
2017-11-15 11:23:29 +0200

    rename labels, create outlet for treeimage

--- ApplePie/ViewController.swift
+++ ApplePie/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var treeImageView: UIImageView!
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
